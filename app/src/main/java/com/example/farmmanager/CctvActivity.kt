8a7d1f8ee0718c277924fe4abce6f2f94b2dd34c
LUC <rndnjswk123@gmail.com>
2022-09-08 21:13:51 +0900

    09.08 수정

--- app/src/main/java/com/example/farmmanager/CctvActivity.kt
+++ app/src/main/java/com/example/farmmanager/CctvActivity.kt
@@ -3,6 +3,7 @@ package com.example.farmmanager
 
 import androidx.appcompat.app.AppCompatActivity //MainActivity가 상속받을 클래스
 import android.os.Bundle //MainActivity가 받을 자료형 클래스
+import android.util.Log
 import com.example.farmmanager.databinding.ActivityCctvBinding //databing(레이아웃 연동)을 위한 클래스
 import android.webkit.WebViewClient
 import com.example.farmmanager.GlobalVariables as G
@@ -12,9 +13,28 @@ class CctvActivity : AppCompatActivity() {
 
     val binding by lazy { ActivityCctvBinding.inflate(layoutInflater) }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        //초기화
+        binding.switchAlert3.isChecked = (G.sirenOption==1)
+
+
+        //[경고 메세지 수신 여부]에 대한 바인딩
+        binding.switchAlert3.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                G.sirenOption = 1
+                Log.d("LOG_[CctvActivity]","[CctvActivity] 사이렌 ON")
+                G.toast("[CctvActivity] 사이렌 ON")
+            }
+            else {
+                G.sirenOption = 0
+                Log.d("LOG_[CctvActivity]","[CctvActivity] 사이렌 OFF")
+                G.toast("[CctvActivity] 사이렌 OFF")
+            }
+        }
 
         binding.videoView.apply {
             webViewClient = WebViewClient()
